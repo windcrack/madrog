fc6380629937ad002b31bb40a331bca17cbfdebb
Saha <windrack@mail.ru>
2019-01-04 14:33:38 +0300

    4-1 14:33

--- js/script.js
+++ js/script.js
@@ -2,4 +2,14 @@ $(document).ready(function(){
 	$(() =>{
 		$('#phone-number').mask('+7(999) 999-99-99');
 	});
+	$('.button-application').click(function(){
+		$('.popup-request').css('display', 'block');
+	});
+	$('.header-nav-call').click(function(){
+		$('.popup-phone').css('display', 'block');
+	});
+	$('.closeBtn').click(function(){
+		$('.popup-request').css('display', 'none');
+		$('.popup-phone').css('display', 'none');
+	});
 });
