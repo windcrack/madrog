4e343317c51abc7ac64f67fb246b97ecfd16b0cb
Saha <windcrack@mail.ru>
2019-01-24 14:01:21 +0300

    repair change block

--- js/script.js
+++ js/script.js
@@ -18,6 +18,7 @@ $(document).ready(function(){
 	}
 	changeBlock.addEventListener('click', e => {
 		let target = e.target;
+			console.log(target);
 		if (target && target.classList.contains('changeContent')) {
 			for(let i = 0; i < change.length; i++){
 				if(target == change[i]){
