338422d36dcf2cd2d0083860f3115143c3dd1041
Saha <windcrack@mail.ru>
2019-01-24 13:57:09 +0300

    setup change block

--- js/script.js
+++ js/script.js
@@ -1,4 +1,32 @@
 $(document).ready(function(){
+	let changeBlock = document.querySelector('.changeBlock'),
+		change = document.querySelectorAll('.changeContent'),
+		content = document.querySelectorAll('.contentDesc');
+	let hideContent = function(h) {
+		for (let i = h; i < content.length; i++){
+			content[i].classList.remove('showDes');
+			content[i].classList.add('hideDes');
+		}
+	}
+	hideContent(1);
+
+	let showContent = function(s) {
+		if(content[s].classList.contains('hideDes')){
+			content[s].classList.remove('hideDes');
+			content[s].classList.add('showDes');
+		}
+	}
+	changeBlock.addEventListener('click', e => {
+		let target = e.target;
+		if (target && target.classList.contains('changeContent')) {
+			for(let i = 0; i < change.length; i++){
+				if(target == change[i]){
+					hideContent(0);
+					showContent(i);
+				}
+			}
+		}
+	});
 	$(() =>{
 		$('#phone-number').mask('+7(999) 999-99-99');
 	});
